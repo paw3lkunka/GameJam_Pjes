65b7fa5e3f038d2c39ba0cc04e6556a48344543d
Ziemniak <potatoosalad@gmail.com>
2020-01-31 21:08:10 +0100

    Switches

---
+++ Assets/Scripts/Triggers/OnOffSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnOffSwitch : MonoBehaviour, ISwitch
+{
+    public bool State { get; private set; }
+
+    public UnityEvent TurnOn;
+    public UnityEvent TurnOff;
+
+    public void Use()
+    {
+        if (State)
+        {
+            TurnOff.Invoke();
+            State = false;
+        }
+        else
+        {
+            TurnOn.Invoke();
+            State = true;
+        }
+    }
+}

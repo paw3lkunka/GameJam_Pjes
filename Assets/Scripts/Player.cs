e4317707d6eed74284660c9d52903bc2bc74420b
Truk4 <starcraft2323@wp.pl>
2020-01-31 20:55:38 +0100

    Some cleaning done.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private NewInput input;
+
+    void Awake()
     {
-        
+        input = new NewInput();
     }
 
-    // Update is called once per frame
     void Update()
     {
         

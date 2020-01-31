b5e0fc2625e8be39469970921f34f4b17a859895
AandrzejMi <andrzej.mi@op.pl>
2020-01-31 22:58:56 +0100

    LevelMAnager

---
+++ Assets/Scripts/Editor/LevelManagerEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelManager))]
+public class LevelManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        // Function buttons
+        bool gravity = LevelManager.instance.Gravity;
+        if( GUILayout.Button( string.Format("Turn {0} Gravity", gravity ? "off" : "on") ) )
+        {
+            LevelManager.instance.Gravity = !gravity;
+        }
+        if (GUILayout.Button("Stop"))
+        {
+            LevelManager.instance.Stop();
+        }
+
+    }
+}

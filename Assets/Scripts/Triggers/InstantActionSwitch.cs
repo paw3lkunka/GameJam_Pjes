22efc01161403949a3b1c4cb6b304bc34c8dcc57
Ziemniak <potatoosalad@gmail.com>
2020-01-31 21:11:35 +0100

    IntantActionSwitch

---
+++ Assets/Scripts/Triggers/InstantActionSwitch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InstantActionSwitch : MonoBehaviour, ISwitch
+{
+    public UnityEvent OnPush;
+    public void Use()
+    {
+        OnPush.Invoke();
+    }
+}

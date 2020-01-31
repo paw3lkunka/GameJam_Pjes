5d379d84bcab93b45222e64ef40674959e047395
Truk4 <starcraft2323@wp.pl>
2020-01-31 21:44:27 +0100

    Update 1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public bool jumpEnabled = false;
+    public bool interactEnabled = true;
+    public bool moveUpEnabled = false;
+    public bool moveDownEnabled = false;
+    public bool moveRightEnabled = false;
+    public bool moveLeftEnabled = false;
+
     private NewInput input;
+    private Vector2 movement;
 
     void Awake()
     {
@@ -14,5 +22,18 @@ public class Player : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnEnable() // Required for NewInput system.
+    {
+        input.Gameplay.MoveUp.performed += ctx =>
+        {
+            movement = ctx.ReadValue<Vector2>();
+        };
+    }
+
+    private void MoveUp()
+    {
+
     }
 }

e21809077756845d5e9e8247463469f95441bd09
Truk4 <starcraft2323@wp.pl>
2020-02-01 17:44:44 +0100

    Pressure plate added.

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
+    [SerializeField]
+    private Sprite plateImage;
+    [SerializeField]
+    private Sprite pressedPlateImage;
     private int itemsPressingCount = 0;
 
     void OnTriggerEnter2D()
@@ -12,6 +16,7 @@ public class PressurePlate : MonoBehaviour
         if(itemsPressingCount == 1)
         {
             GetComponent<OnOffSwitch>().On();
+            GetComponent<SpriteRenderer>().sprite = pressedPlateImage;
         }
     }
 
@@ -21,6 +26,7 @@ public class PressurePlate : MonoBehaviour
         if(itemsPressingCount == 0)
         {
             GetComponent<OnOffSwitch>().Off();
+            GetComponent<SpriteRenderer>().sprite = plateImage;
         }
     }
 }

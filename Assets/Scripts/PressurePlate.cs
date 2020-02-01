9f536ea29f507f8b52c98eaf0528de0a434811f3
Truk4 <starcraft2323@wp.pl>
2020-02-01 16:52:19 +0100

    Added basics of pressure plates system.

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    private int itemsPressingCount = 0;
+
+    void OnTriggerEnter2D()
+    {
+        itemsPressingCount++;
+        if(itemsPressingCount == 1)
+        {
+            GetComponent<OnOffSwitch>().On();
+        }
+    }
+
+    void OnTriggerExit2D()
+    {
+        itemsPressingCount--;
+        if(itemsPressingCount == 0)
+        {
+            GetComponent<OnOffSwitch>().Off();
+        }
+    }
+}

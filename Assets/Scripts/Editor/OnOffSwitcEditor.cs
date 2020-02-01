308b9bd96b06769cecc8fc2814e5c954d4280dfb
AandrzejMi <andrzej.mi@op.pl>
2020-02-01 19:08:23 +0100

    level 5, on/off switch custom editor

---
+++ Assets/Scripts/Editor/OnOffSwitcEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(OnOffSwitch))]
+public class OnOffSwitchEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Turn on"))
+        {
+            (target as OnOffSwitch).On();
+        }
+        if (GUILayout.Button("Turn off"))
+        {
+            (target as OnOffSwitch).Off();
+        }
+        if (GUILayout.Button("Use"))
+        {
+            (target as OnOffSwitch).Use();
+        }
+
+        GUILayout.EndHorizontal();
+
+        base.OnInspectorGUI();
+    }
+}

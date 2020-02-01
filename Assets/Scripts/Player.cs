b45def36c30c6b463eef29dda6b9582f56697532
PawelKunka <pawelkun@gmail.com>
2020-02-01 02:20:51 +0100

    Changed switchesInRange protection level to public

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,12 +17,13 @@ public class Player : MonoBehaviour
     public bool moveRightEnabled = false;
     public bool moveLeftEnabled = false;
 
+    [HideInInspector]
+    public List<Switch> switchesInRange;
+
     private NewInput input;
     private Vector2 simpleMove;
 
     private new Rigidbody2D rigidbody;
-
-    private List<Switch> switchesInRange;
 
     #region MonoBehaviourMethods
 
